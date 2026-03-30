cec36a0fcc0e05a4d0b49789db8d92da32a293ba
nlehuythai <nlht081005@gmail.com>
2026-03-30 20:28:19 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,2 @@
 import "./globals.css";
 
-export const metadata = {
-    title: "MyStore",
-    description: "Thực hành Next.js buổi 1",
-};
-
-export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body>
-                {children}
-            </body>
-        </html>
-    );
-}
