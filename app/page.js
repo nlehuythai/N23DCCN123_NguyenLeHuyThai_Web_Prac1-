7ca64695b50ca0ee48895c2cf3189d0aaef75331
nlehuythai <nlht081005@gmail.com>
2026-03-30 20:27:38 +0700

    Update page.js
    Removed the getProducts function and the HomePage component that fetched and displayed products.

--- app/page.js
+++ app/page.js
@@ -1,28 +1,3 @@
 import Navbar from "@/Component/Navbar";
 import ProductCard from "@/Component/ProductCard";
 
-async function getProducts() {
-    const res = await fetch('https://fakestoreapi.com/products');
-    if (!res.ok) throw new Error('Failed to fetch data');
-    return res.json();
-}
-
-export default async function HomePage() {
-    const products = await getProducts();
-
-    return (
-        <main className="min-h-screen bg-gray-50">
-            <Navbar />
-            <div className="max-w-7xl mx-auto">
-                <h1 className="text-3xl font-bold p-6 text-center">Our Products</h1>
-
-                {/* Grid Layout Responsive */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
-                    {products.map(item => (
-                        <ProductCard key={item.id} product={item} />
-                    ))}
-                </div>
-            </div>
-        </main>
-    );
-}
